90f7cd19935b6829d56140366e7601cde76290ab
Vincent Pauley <vinniepauley@gmail.com>
2017-01-04 23:17:02 -0600

    create from FE working, no security yet

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
-var app = angular.module('hockey', []);
+(() => {
+    var app = angular.module('hockey', []);
+
+    app.controller('createNewTeam', function($http) {
+        var vm = this;
+        vm.new_team = {
+            name : "",
+            state : "",
+            city : "",
+            league : "",
+            cups : 0,
+            originYear : 0
+        };
+
+        vm.submit_new_team = () => {
+            $http({
+                url : '/add-team',
+                method : 'POST',
+                data : {
+                    new_team : vm.new_team
+                }
+            });
+        }
+    });
+})();
--- server.js
+++ server.js
@@ -1,37 +1,41 @@
 var express = require('express'),
     mongodb = require('mongodb'),
     mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
     app = express(),
     port = 3000;
 
 // models
 var Team = require('./models/Team.model');
 
-
-
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 var db = 'mongodb://localhost/hockey';
 mongoose.connect(db);
 
-/*
-var newTeam = new Team();
-    newTeam.name = "Blackhawks";
-    newTeam.state = "Illinois";
-    newTeam.city = "Chicago";
-    newTeam.league = "NHL";
-    newTeam.cups = 6;
-    newTeam.originYear = 1926;
 
-newTeam.save(function(err, newTeam) {
-    if(err) {
-        console.error(err);
-    } else {
-        console.log('SUCCESS');
-        console.log(newTeam);
-    }
+
+app.post('/add-team', function(req, res) {
+    console.log('client requesting to add team');
+
+    var newTeam = new Team();
+        newTeam.name = req.body.new_team.name;
+        newTeam.state = req.body.new_team.state;
+        newTeam.city = req.body.new_team.city;
+        newTeam.league = req.body.new_team.league;
+        newTeam.cups = req.body.new_team.cups;
+        newTeam.originYear = req.body.new_team.originYear;
+
+    newTeam.save(function(err, newTeam) {
+        if(err) {
+            console.error(err);
+        } else {
+            console.log('SUCCESS');
+            console.log(newTeam);
+        }
+    });
 });
-*/
 
 
 
